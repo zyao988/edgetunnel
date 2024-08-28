533f5d9547498ab8fa01bd90b78ab99275545e71
zyao988 <121565928+zyao988@users.noreply.github.com>
2024-08-28 16:48:57 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '451515ff-cbd0-47b6-bc23-cec9c43a1546';
+let userID = '6d3ad578-22ad-48f5-ab33-8f32742782df';
 
 let proxyIP = '';// 小白勿动，该地址并不影响你的网速，这是给CF代理使用的。'cdn.xn--b6gac.eu.org, cdn-all.xn--b6gac.eu.org, workers.cloudflare.cyou'
 
